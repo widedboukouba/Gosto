326e2021961601b7c71856f917e471c5ab58e6ca
widedboukouba <widedboukouba64@gmail.com>
2023-08-16 16:02:00 +0200

    first

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © GOSTO . Powered by WidedBoukouba.</p>
+      </footer>
+    </>
+  )
+}
